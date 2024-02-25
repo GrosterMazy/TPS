fd497e1d4a8d5d5052f970598eb540d7a7caf346
GrosterMazy <78735997+GrosterMazy@users.noreply.github.com>
2024-02-25 14:07:51 +0300

    Fireball Update
    Added:
    - Fireball shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float Speed;
+    public float Lifetime;
+
+    void DestroyBullet() {
+        Destroy(this.gameObject);
+    }
+
+    void Start() {
+        Invoke("DestroyBullet", this.Lifetime);
+    }
+
+    void OnCollisionEnter(Collision collider) {
+        this.DestroyBullet();
+    }
+
+    void FixedUpdate() {
+        this.transform.position += this.transform.forward * this.Speed * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerCast.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCast : MonoBehaviour {
+
+    public GameObject fireballPrefab;
+    public Transform hand;
+    public Transform aim;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            hand.LookAt(aim.position);
+            Instantiate(this.fireballPrefab, this.hand.position+this.hand.forward, this.hand.rotation);
+        }
+    }
+}
