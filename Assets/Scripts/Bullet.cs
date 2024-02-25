1c135a7c53df720227beaba66fbc120077555936
GrosterMazy <78735997+GrosterMazy@users.noreply.github.com>
2024-02-25 18:56:07 +0300

    Damage Update
    - Now you can kill enemy with fireballs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-    public float Speed;
-    public float Lifetime;
+    public float speed;
+    public float lifetime;
+
+    public float damage;
 
     void DestroyBullet() {
         Destroy(this.gameObject);
     }
 
     void Start() {
-        Invoke("DestroyBullet", this.Lifetime);
+        Invoke("DestroyBullet", this.lifetime);
     }
 
     void OnCollisionEnter(Collision collider) {
+        EnemyHP comp = collider.gameObject.GetComponent<EnemyHP>();
+        if (comp != null) comp.DealDamage(this.damage);
         this.DestroyBullet();
     }
 
     void FixedUpdate() {
-        this.transform.position += this.transform.forward * this.Speed * Time.fixedDeltaTime;
+        this.transform.position += this.transform.forward * this.speed * Time.fixedDeltaTime;
     }
 }
---
+++ Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour {
+    public float value = 100;
+
+    public void DealDamage(float damage) {
+        this.value -= damage;
+        if (this.value <= 0) Destroy(this.gameObject);
+    }
+}
